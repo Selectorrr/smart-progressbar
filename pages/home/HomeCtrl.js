d96511c1131e7bd6bd3f06145895779c70aac7d3
stepan litvinov <stepan_litvinov@epam.com>
2017-02-24 12:55:06 +0300

    init

---
+++ pages/home/HomeCtrl.js
@@ -0,0 +1,8 @@
+/**
+*/
+
+'use strict';
+
+angular.module('myApp').controller('HomeCtrl', ['$scope', 'selSmartProgressbar', function($scope, selSmartProgressbar) {
+	//TODO - put any directive code here
+}]);
